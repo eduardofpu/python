ef86eeb23a6a41df3b9c8a9dd40702346d060128
eduardo <eduardo27_minotauro>
2019-08-17 10:32:23 -0300

    email

---
+++ exemplos/email/envio.py
@@ -0,0 +1,33 @@
+import smtplib
+
+#Email recipiente
+meu_email = "eduardo.r.delfino@gmail.com"
+
+#Email destinatario
+enviar_para = "eduardo27_minotauro@hotmail.com"
+
+#servidor SMTP
+smtp = "smtp.gmail.com"
+
+#Servidor SMTP
+server = smtplib.SMTP(smtp,587)
+
+#Define conexoes usando tls
+server.starttls()
+
+#Realiza login no servidor de SMTP passando usuario e senha
+server.login(meu_email, open('senha.txt').read().strip())
+
+
+#Corpo do email
+msg = '''
+      Envio de e-mail teste
+      Curoso Phytonicos
+      Bem-vindo a aula de envio de e-mail
+
+'''
+#Enviar o email
+server.sendmail(meu_email, enviar_para, msg)
+
+#Fechar a conexao
+server.quit()
